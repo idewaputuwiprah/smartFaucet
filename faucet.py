e9e2ec6f2bca16011e590bf22d94fe30fbf6a033
idewaputuwiprah <adit@klikpeta.com>
2023-12-03 12:31:26 +0700

    init project

---
+++ faucet.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+
+class Faucet:
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(12, GPIO.IN) #==sets GPIO12 as INPUT sensor tutup
+        GPIO.setup(16, GPIO.IN) #==sets GPIO16 as INPUT sensor buka
+        GPIO.setup(20, GPIO.OUT) #==sets GPIO20 as OUTPUT H-Bridge
+        GPIO.setup(21, GPIO.OUT) #==sets GPIO21 as OUTPUT H-Bridge
+
+    def open_faucet():
+        GPIO.output(20, True) #==  mengirimkan biner 1 ke pin GPIO 20 (membuka keran)
+        GPIO.output(21, False) #== mengirim biner 0 ke pin GPIO 21 (membuka keran)
+
+    def close_faucet():
+        GPIO.output(20, False) #==  mengirimkan biner 1 ke pin GPIO 20 (menutup keran)
+        GPIO.output(21, True) #== mengirim biner 0 ke pin GPIO 21 (menutup keran)
+
+    def off_motor():
+        GPIO.output(20, True) #==  mengirimkan biner 1 ke pin GPIO20 (power motor off)
+        GPIO.output(21, True) #== mengirim biner 0 ke pin GPIO 21 (power motor off)
+
+    
---
+++ faucet_sensor.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+
+class FaucetSensor:
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        #or to use the physical pin numbers (1-40) instead of GPIO number
+        #GPIO.setmode(GPIO.BOARD)
+        # setting up pins as I/O
+        #== GPIO 12 (PIN 32) input sensor kondisi tutup ====
+        #== GPIO 16 (PIN 36) input sensor kondisi buka  ====
+        GPIO.setup(12, GPIO.IN) #sets GPIO12 as INPUT
+        GPIO.setup(16, GPIO.IN) #sets GPIO16 as INPUT
+        GPIO.setup(20, GPIO.OUT) #sets GPIO20 as OUTPUT
+        GPIO.setup(21, GPIO.OUT) #sets GPIO21 as OUTPUT
+
+    def read_sensor_data():
+        aboveSensor = GPIO.input(12) # membaca kondisi pin GPIO12 sensor tutup (Biner)
+        belowSensor = GPIO.input(16) #membaca kondisi pin GPIO16 sensor buka (Biner)
+        return SensorOutput(aboveSensor, belowSensor)
+    
+class SensorOutput:
+    
+    def __init__(self, aboveSensor, belowSensor):
+        self.aboveSensor = aboveSensor
+        self.belowSensor = belowSensor
